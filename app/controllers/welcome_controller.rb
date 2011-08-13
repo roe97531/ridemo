fb5747ab8e216336b9e6085be3e679dafbcd616c
Roberto V. Elrico <roe97531@yahoo.com>
2011-08-13 09:29:27 -0500

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+
+  # zyx roe: orig, generated empty index action
+  # this will default to loading the index.html.erb file from the
+  # views/welcome dir
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = 'c2f6755071e7c455052b4eb4a0be9a40f791123ae46d8c1534b2b50b3ea8694ccec6bab21c8d08a6f673c1e8052fdeac1b39ffc66070acc0e081108f1832ce53'
